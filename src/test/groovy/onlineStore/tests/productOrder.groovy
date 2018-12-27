e89f02f2fb6c602e06765beee289ad290df6ccc8
k.bikineev <k.bikineev@tinkoff.ru>
2018-12-27 23:52:39 +0300

    Дофига всего

--- src/test/groovy/onlineStore/tests/productOrder.groovy
+++ src/test/groovy/onlineStore/tests/productOrder.groovy
@@ -1,26 +1,60 @@
 package onlineStore.tests
 
+
 import com.codeborne.selenide.Selenide
 import org.junit.Test
-import com.codeborne.selenide.SelenideElement
 
 import static com.codeborne.selenide.Selenide.$
-import static com.codeborne.selenide.Selenide.$$
-import static com.codeborne.selenide.Selenide.$$x
+import static com.codeborne.selenide.Selenide.$x
 
 class productOrder {
     @Test
     public void testProductOrder() {
         Selenide.open("http://litecart.stqa.ru/en/")
 
-        def product = $(".product")
 
+        def product = $(".product")
+        product.shouldBe(com.codeborne.selenide.Condition.visible)
         product.click()
 
-        def productPrice = $$x("//span[contains(@class,'price')][contains(@itemprop,'price')]")
+        $("[itemprop='price']").shouldBe(com.codeborne.selenide.Condition.visible)
+        def currentPrice = $("[itemprop='price']").text
 
-        def orderPrice = orderPrice + productPrice.getValue()
+        if ($x("//select[@name = 'options[Size]']").exists()) {
+            def productSize = $x("//select[@name = 'options[Size]']")
+            productSize.selectOptionContainingText("Small")
+        }
 
-        
+        def addToCartButton = $("[name='buy_now_form'] button")
+        addToCartButton.click()
+
+        $("#cart .quantity").shouldHave(com.codeborne.selenide.Condition.exactText("1"))
+
+        def cart = $("#cart")
+        cart.click()
+
+        def cartPrice = $("[name='cart_form'] div p:nth-of-type(2)")
+        cartPrice.shouldBe(com.codeborne.selenide.Condition.visible)
+        assert currentPrice == cartPrice.text
+
+        fillAddressForm("firstname", "1")
+        fillAddressForm("lastname", "2")
+        fillAddressForm("address1", "4")
+        fillAddressForm("postcode", "420066")
+        fillAddressForm("city", "Midnight")
+        fillAddressForm("phone", "88005556565")
+
+        def confirmButton = $x("//button[@name='confirm_order']")
+        confirmButton.shouldBe(com.codeborne.selenide.Condition.visible)
+        confirmButton.click()
+
+        def successMessage = $("#box-order-success")
+        successMessage.shouldBe(com.codeborne.selenide.Condition.visible)
+    }
+
+    static fillAddressForm(String fieldname, fieldValue) {
+        def billingAddress = $(".billing-address")
+        def addressField = billingAddress.$x(".//input[@name = '$fieldname']")
+        addressField.value = fieldValue
     }
 }
